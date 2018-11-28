c202568e0fae2ca0fa72a18e29b2d4214f99e4ea
vin-ni <vinzenz@weboffice.de>
2018-11-28 15:50:23 +0100

    added normalize function

--- javascript.js
+++ javascript.js
@@ -115,3 +115,16 @@ function checkPswd() {
         alert("Passwords do not match.");
     }
 }
+
+/* ====== Normalize / Map Value Range to different Range ======*/
+/* (currentValue, currentMin, currentMax, mapMin, mapMax) */
+function normalize(v,vmin,vmax,tmin, tmax){
+
+	var nv = Math.max(Math.min(v,vmax), vmin);
+	var dv = vmax-vmin;
+	var pc = (nv-vmin)/dv;
+	var dt = tmax-tmin;
+	var tv = tmin + (pc*dt);
+	return tv;
+
+}
