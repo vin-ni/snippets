475513c119813522c58ea80dd0fb65c82df8ad6b
Vinzenz Aubry <vinzenzaubry@gmail.com>
2021-03-01 00:33:18 +0100

    fixt port naming

--- bases/nuxt base global scss font import yarn/ecosystem.config.js
+++ bases/nuxt base global scss font import yarn/ecosystem.config.js
@@ -1,14 +1,14 @@
 module.exports = {
   apps: [
     {
-      name: 'yarn-template',
+      name: 'drifter.augmented.audio',
       exec_mode: 'cluster',
       instances: 'max', // Or a number of instances
       script: './node_modules/nuxt/bin/nuxt.js',
       args: 'start',
       env: {
-        HOST: 'localhost',
-        PORT: 3000,
+        NUXTHOST: 'localhost',
+        NUXTPORT: 3004,
       },
     },
   ],
