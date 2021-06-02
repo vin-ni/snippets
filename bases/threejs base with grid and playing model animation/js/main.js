5132a8acabc55953f798986862cfdfc80ef54277
Vinzenz Aubry <vinzenzaubry@gmail.com>
2021-06-02 16:25:47 +0200

    added animation

---
+++ bases/threejs base with grid and playing model animation/js/main.js
@@ -0,0 +1,152 @@
+/* eslint-disable */
+
+function SolarSystem() {
+  this.params = {
+    width: window.innerWidth,
+    height: window.innerHeight,
+
+    gridColor: 'white',
+    gridOpacity: 0.5,
+    gridSize: 400,
+    gridDivisions: 20,
+  };
+
+  this.init();
+}
+
+SolarSystem.prototype.init = function () {
+  this.setupRender();
+  this.setupLights();
+  this.addAxesHelper();
+  this.setupControls();
+  this.setupStats();
+  this.loadModel();
+  this.render();
+};
+
+SolarSystem.prototype.setupRender = function () {
+  this.scene = new THREE.Scene();
+
+  this.camera = new THREE.PerspectiveCamera(40, this.params.width / this.params.height, 0.1, 1000);
+  this.camera.position.set(0, 250, 400);
+  this.camera.lookAt(this.scene.position);
+
+  this.renderer = new THREE.WebGLRenderer({ antialias: true });
+  this.renderer.setSize(this.params.width, this.params.height);
+  // this.renderer.setClearColor(this.params.backgroundColor);
+  //raycaster = new THREE.Raycaster();
+
+  document.body.appendChild(this.renderer.domElement);
+  window.addEventListener('resize', this.resize.bind(this), false);
+
+  this.clock = new THREE.Clock();
+};
+
+SolarSystem.prototype.resize = function () {
+  console.log('resize');
+  this.params.width = window.innerWidth;
+  this.params.height = window.innerHeight;
+
+  let W = this.params.width;
+  let H = this.params.height;
+
+  this.camera.aspect = W / H;
+  this.camera.updateProjectionMatrix();
+  this.renderer.setSize(W, H);
+  this.controls.handleResize();
+};
+
+SolarSystem.prototype.setupLights = function () {
+  var light_amb = new THREE.AmbientLight(0xffffff, 2);
+
+  var light_hem = new THREE.HemisphereLight(0xffffcc, 0x222200, 3);
+  light_hem.position.setY(15);
+
+  var light_dir = new THREE.DirectionalLight();
+
+  this.scene.add(light_amb, light_hem, light_dir);
+};
+
+SolarSystem.prototype.addAxesHelper = function () {
+  var gridHelper = new THREE.GridHelper(this.params.gridSize, this.params.gridDivisions, this.params.gridColor, this.params.gridColor);
+  gridHelper.material.transparent = true;
+  gridHelper.material.opacity = this.params.gridOpacity;
+  gridHelper.position.y = 0;
+  this.scene.add(gridHelper);
+};
+
+SolarSystem.prototype.setupControls = function () {
+  this.controls = new THREE.TrackballControls(this.camera, this.renderer.domElement);
+  this.controls.rotateSpeed = 1.0;
+  this.controls.zoomSpeed = 1.2;
+  this.controls.panSpeed = 0.8;
+
+  this.controls.noZoom = false;
+  this.controls.noPan = false;
+
+  this.controls.staticMoving = true;
+  this.controls.dynamicDampingFactor = 0.3;
+
+  this.controls.keys = [65, 83, 68];
+
+  // controls.addEventListener( 'change', render );
+};
+
+SolarSystem.prototype.setupStats = function () {
+  this.renderStats = new Stats();
+  this.renderStats.domElement.style.position = 'absolute';
+  this.renderStats.domElement.style.top = '1px';
+  this.renderStats.domElement.style.zIndex = 100;
+
+  this.renderStats.domElement.hidden = false;
+  document.body.appendChild(this.renderStats.domElement);
+};
+
+SolarSystem.prototype.loadModel = function () {
+  var self = this;
+  const loader = new THREE.GLTFLoader();
+
+  // Load a glTF resource
+  loader.load(
+    // resource URL
+    './model/Pendulum.glb',
+    // called when the resource is loaded
+    function (gltf) {
+      self.scene.add(gltf.scene);
+      gltf.scene.scale.set(10, 10, 10);
+      self.startAnimation(gltf);
+
+      //   gltf.animations; // Array&lt;THREE.AnimationClip&gt;
+      //   gltf.scene; // THREE.Group
+      //   gltf.scenes; // Array&lt;THREE.Group&gt;
+      //   gltf.cameras; // Array&lt;THREE.Camera&gt;
+      //   gltf.asset; // Object
+    },
+    // called while loading is progressing
+    function (xhr) {
+      console.log((xhr.loaded / xhr.total) * 100 + '% loaded');
+    },
+    // called when loading has errors
+    function (error) {
+      console.log('An error happened');
+    }
+  );
+};
+
+SolarSystem.prototype.startAnimation = function (model) {
+  this.mixer = new THREE.AnimationMixer(model.scene);
+  const clipAction = this.mixer.clipAction(model.animations[0]);
+  clipAction.play();
+};
+
+SolarSystem.prototype.render = function () {
+  requestAnimationFrame(this.render.bind(this));
+  this.controls.update();
+  this.renderer.render(this.scene, this.camera);
+
+  if (this.mixer) {
+    deltaSeconds = this.clock.getDelta();
+    this.mixer.update(deltaSeconds);
+  }
+  this.renderStats.update();
+};
